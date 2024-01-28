62a8ff409392811059e50f5b42c05910dc6dac21
PhyoZayHtike <143343820+PhyoZayHtike@users.noreply.github.com>
2024-01-28 21:56:45 +0630

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555
 
-export const mongoDBURL = 'mongodb+srv://phyozayhtike:phyozayhtike0979@book-store-mern.0hmlqhs.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const mongoDBURL = ''
 
